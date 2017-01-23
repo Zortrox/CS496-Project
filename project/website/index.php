15d8d0f3d7da0590765e3a5c86f0ff297384a1ff
Christopher Goulet <christophergoulet@outlook.com>
2017-01-23 11:23:42 -0600

    Added Index.php

---
+++ project/website/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chris
+ * Date: 1/23/2017
+ * Time: 11:05 AM
+ */
+?>
+
+
+<h<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>CS496 Project</title>
+
+    <!-- Main Style -->
+    <link href="css/main.css" rel="stylesheet">
+
+
+</head>
+<body>
+<h1>Hello, <?php echo "world!!!"; ?></h1>
+
+</body>
+</html>
