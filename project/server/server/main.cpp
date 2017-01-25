418775df6ba4a2e6d9a90e7a32e4b454c7189a83
Zortrox <zortroxofdoom@gmail.com>
2017-01-25 10:51:22 -0600

    added basic Visual Studio structure with main.cpp

---
+++ project/server/server/main.cpp
@@ -0,0 +1,12 @@
+/*Game server for communicating with the website, host screen,
+and controllers*/
+
+#include <iostream>
+
+int main(int argc, char* argv) {
+
+	std::cout << "Hello, world" << std::endl;
+	std::cin.ignore();
+
+	return 0;
+}
