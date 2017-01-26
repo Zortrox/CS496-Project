bb4ede8891d954ef3f8b3eaa57da2d698a26fa73
Christopher Goulet <christophergoulet@outlook.com>
2017-01-25 22:54:38 -0600

    Created Simple Menu to get started

--- project/website/index.php
+++ project/website/index.php
@@ -12,7 +12,8 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>CS496 Project</title>
 
-
+    <!-- Font Awesome-->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
     <!-- Bootstrap Stylesheets   -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Main Style -->
@@ -22,7 +23,28 @@
 </head>
 <body>
 
-    <h1>Hello, <?php echo "world!!!"; ?></h1>
+    <div class="container-fluid">
+        <div class="menu-container">
+            <div class="col-md-4 well">
+                <div class="col-row">
+                    <h1 class="text-center">
+                        <i class="fa fa-child" aria-hidden="true"></i> Party Screen <i class="fa fa-child" aria-hidden="true"></i>
+                    </h1>
+                </div>
+                <div class="col-row">
+                    <button type="button" class="btn add-margin-vertical btn-primary center-block">Host Game</button>
+                </div>
+
+                <div class="col-row">
+                    <button type="button" class="btn add-margin-vertical btn-primary center-block">Join Game</button>
+                </div>
+                <div class="col-row">
+                    <button type="button" class="btn add-margin-vertical btn-info center-block">Information</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <script src="js/jquery-3.1.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
