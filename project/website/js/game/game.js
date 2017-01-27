180ab0339301dcc4cc25a3a4e7d91aa16766d723
Connor <connormbrooks@gmail.com>
2017-01-26 21:16:31 -0600

    Added game updating to game API

--- project/website/js/game/game.js
+++ project/website/js/game/game.js
@@ -1,12 +1,20 @@
 //Game API
 
+//-------------------------------------------------------------------------------------
+
+//DynamicObject
+//Object that represents a single moving/dynamic sprite
 function DynamicObject(drawFunction, canv, params){
-	//params should be a list of ids that correspond to the game's parameter id list
-	this.params = params;
+	this.params = params; //params should be a list of ids that correspond to the game's parameter id list
 	this.canv = canv;
 	this.draw = drawFunction;
+	this.expired = false; //set expired to true when object is ready to be deleted
 }
 
+//-------------------------------------------------------------------------------------
+
+//Canvas Object
+//Object that represents a single display region
 function Canvas(canv, game){
 	this.canv = canv;
 	this.game = game;
@@ -20,14 +28,43 @@ function Canvas(canv, game){
 	this.addGameObject = function(obj){
 		this.dynamicObjects.push(obj);
 	}
+
+	this.updateCanvas = function(){
+		this.resetToBaseState();
+		this.drawObjects();
+	}
+
+	this.resetToBaseState = function(){
+		this.canv.ctx.putImageData(this.baseState, 0, 0);
+	}
+
+	this.drawObjects = function(){
+		var i = 0;
+		var boundary = this.dynamicObjects.length;
+		while(i < boundary){
+			if(this.dynamicObjects[i].expired){
+				this.dynamicObjects.splice(i,1);
+				boundary--;
+			} else {
+				this.dynamicObjects[i].draw();
+				i++;
+			}
+		}
+	}
 }
 
+//-------------------------------------------------------------------------------------
+
+//Game Object
+//Master object (singleton design model?) for a unique game instance
 function Game(id){
 	this.id = id;
-	this.params = [];
+	this.params = {};
 	this.canvs = [];
 	this.htmlObjects = [];	//non-canvas HTML elements
-	this.players = [];
+	this.frame_rate = 10;
+	this.active = true;
+	//this.players = [];
 	this.htmlBod = document.getElementsByTagName("body")[0];
 	
 	//all HTML element insertions (Canvas or other) insert the new element as the first element in body
@@ -49,5 +86,36 @@ function Game(id){
 
 	this.addParam = function(id, defValue){
 		this.params[""+id] = defValue;
-	}	
+	}
+
+	this.setFrameRate(num){
+		this.frame_rate = num;
+	}
+
+	this.updateParams = function(){
+		//TODO: Communicate with server to obtain updated values of all params
+	}
+
+	this.gameRefresh = function(){
+		for(canv in canvs){
+			canv.updateCanvas();
+		}
+	}
+
+	this.startGame = function(){
+		var obj = this;
+		setTimeout(function(){
+			obj.gameLoop(obj);
+		}, obj.frame_rate);
+	}
+
+	this.gameLoop = function(obj){
+		if(obj.active){
+			obj.updateParams();
+			obj.gameRefresh();
+			setTimeout(function(){
+				obj.update(obj);
+			}, obj.frame_rate);
+		}
+	}
 }
