8037144444bbdc540f1e5054ed290b1f2bc82ba9
Jared <jareda.prince@ymail.com>
2017-01-31 12:11:09 -0600

    added an area on the site to view game descriptions/controls

--- project/website/elements/info-modal.php
+++ project/website/elements/info-modal.php
@@ -23,7 +23,7 @@
 
                 <h2>How to Play</h2>
                 <p>
-                    In order to host a game, visit this site with the device on which you want to display the game. Choose the host game button and then choose from our selection of games. If you wish to resrict the game to your friends, select the private option. You will be given a code to allow others to join. Select start to begin the game. To join a game, visit the site on your smartphone and select the join button. If you wish to join a specific game, enter the host's code in the space provided. To join a random game, select the game you wish to play and click search. When you are connected to a game, your phone will display the controller for that game. Have fun!
+                    In order to host a game, visit this site with the device on which you want to display the game. Choose the host game button and then choose from our selection of games. If you wish to resrict the game to your friends, select the private option. You will be given a code to allow others to join. Select start to begin the game. To join a game, visit the site on your smartphone and select the join button. If you wish to join a specific game, enter the host's code in the space provided. To join a random game, select the game you wish to play and click search. When you are connected to a game, your phone will display the controller for that game. Click on the games button from the main menu to see a desciption of each of the games and learn how to play. Have fun!
                 </p>
 
                 <h2>Credits</h2>
--- project/website/js/menu.js
+++ project/website/js/menu.js
@@ -8,16 +8,31 @@
 
 // Create Click Handlers for Buttons
 document.getElementById("host-btn").onclick = function (e) {
-
+    show('host-menu');
+    hide('main-menu');
 };
 
 
 document.getElementById("join-btn").onclick = function (e) {
-
+    show('join-menu');
+    hide('main-menu');
 };
 
+document.getElementById("games-btn").onclick = function (e) {
+    show('game-menu');
+    hide('main-menu');
+};
 
 document.getElementById("info-btn").onclick = function (e) {
 
 };
 
+function hide(name){
+    var menu = document.getElementById(name);
+    menu.style.display = "none";
+}
+
+function show(name){
+    var menu = document.getElementById(name);
+    menu.style.display = "block";
+}
