d74ccdd37dfcb094d850fd9cfc34cd9991542776
Connor <connormbrooks@gmail.com>
2017-04-12 14:59:57 -0500

    Finished unstyled but functional controller

--- project/website/js/controller/controllers/classic.js
+++ project/website/js/controller/controllers/classic.js
@@ -15,9 +15,10 @@ Data packet:
 Data packets are sent on button change
 */
 
+var controller;
 
 function setupController(){
-	var controller = new Controller();
+	controller = new Controller();
 	controller.data["up"] = 0;
 	controller.data["down"] = 0;
 	controller.data["left"] = 0;
@@ -25,108 +26,112 @@ function setupController(){
 	controller.data["a"] = 0;
 	controller.data["b"] = 0;
 
-	var css = ' \
-	+ .arrowUp { \
-	  width: 0;  \
-	  height: 0;  \
-	  border-style: 10vw solid; \
-	  border: transparent transparent transparent black \
-	} \
-		 \
-	.arrowDown { \
-	  width: 0;  \
-	  height: 0;  \
-	  border-style: 10vw solid; \
-	  border: transparent transparent black transparent \
-	} \
-		\
-	.arrowRight {\
-	  width: 0; \
-	  height: 0; \
-	  border-style: 10vw solid; \
-	  border: transparent black transparent transparent \
-	}\
-	\
-	.arrowLeft {\
-	  width: 0; \
-	  height: 0; \
-	  border-style: 10vw solid; \
-	  border: black transparent transparent transparent \
-	}\
+var css = ' \
 	.circle { \
-		width: 10vw;\
-		height: 10vw;\
+		text-align: center; \
+		width: 5vh;\
+		height: 5vh;\
 		border-radius: 50%;\
+		border-style: solid \
 	}';
 	controller.setCSS(css);
-	var leftArrowDiv = document.createElement("div");
-	leftArrowDiv.className = "arrowLeft";
-	$(leftArrowDiv).bind("touchstart mousedown", function() {
-		controller.data["left"] = 1;
-		controller.sendState();
-	});
-	$(leftArrowDiv).bind("touchend mouseup", function() {
-		controller.data["left"] = 0;
-		controller.sendState();
-	});
+	var leftArrowDiv = document.createElement("i");
+	leftArrowDiv.className = "fa fa-arrow-left";
+	leftArrowDiv.style = "font-size: 40px;";
+	leftArrowDiv.setAttribute("onmousedown", "leftClick()");
+	leftArrowDiv.setAttribute("onmouseup", "leftRelease()");
 	controller.addHTMLObject(leftArrowDiv);
-	var rightArrowDiv = document.createElement("div");
-	rightArrowDiv.className = "arrowRight";
-	$(rightArrowDiv).bind("touchstart mousedown", function() {
-		controller.data["right"] = 1;
-		controller.sendState();
-	});
-	$(rightArrowDiv).bind("touchend mouseup", function() {
-		controller.data["right"] = 0;
-		controller.sendState();
-	});
+	var rightArrowDiv = document.createElement("i");
+	rightArrowDiv.style = "font-size: 40px;";
+	rightArrowDiv.className = "fa fa-arrow-right";
+	rightArrowDiv.setAttribute("onmousedown", "rightClick()");
+	rightArrowDiv.setAttribute("onmouseup", "rightRelease()");
 	controller.addHTMLObject(rightArrowDiv);
-	var upArrowDiv = document.createElement("div");
-	upArrowDiv.className = "arrowUp";
-	$(upArrowDiv).bind("touchstart mousedown", function() {
-		controller.data["up"] = 1;
-		controller.sendState();
-	});
-	$(upArrowDiv).bind("touchend mouseup", function() {
-		controller.data["up"] = 0;
-		controller.sendState();
-	});
+	var upArrowDiv = document.createElement("i");
+	upArrowDiv.style = "font-size: 40px;";
+	upArrowDiv.className = "fa fa-arrow-up";
+	upArrowDiv.setAttribute("onmousedown", "upClick()");
+	upArrowDiv.setAttribute("onmouseup", "upRelease()");
 	controller.addHTMLObject(upArrowDiv);
-	var downArrowDiv = document.createElement("div");
-	downArrowDiv.className = "arrowDown";
+	var downArrowDiv = document.createElement("i");
+	downArrowDiv.style = "font-size: 40px;";
+	downArrowDiv.className = "fa fa-arrow-down";
 	controller.addHTMLObject(downArrowDiv);
-	$(downArrowDiv).bind("touchstart mousedown", function() {
-		controller.data["down"] = 1;
-		controller.sendState();
-	});
-	$(downArrowDiv).bind("touchend mouseup", function() {
-		controller.data["down"] = 0;
-		controller.sendState();
-	});
+	downArrowDiv.setAttribute("onmousedown", "downClick()");
+	downArrowDiv.setAttribute("onmouseup", "downRelease()");
 	var aDiv = document.createElement("div");
 	aDiv.className = "circle";
 	aDiv.innerHTML = "A";
-	$(aDiv).bind("touchstart mousedown", function() {
-		controller.data["a"] = 1;
-		controller.sendState();
-	});
-	$(aDiv).bind("touchend mouseup", function() {
-		controller.data["a"] = 0;
-		controller.sendState();
-	});
+	aDiv.setAttribute("onmousedown", "aClick()");
+	aDiv.setAttribute("onmouseup", "aRelease()");
 	controller.addHTMLObject(aDiv);
 	var bDiv = document.createElement("div");
 	bDiv.className = "circle";
 	bDiv.innerHTML = "B";
-	$(bDiv).bind("touchstart mousedown", function() {
-		controller.data["b"] = 1;
-		controller.sendState();
-	});
-	$(bDiv).bind("touchend mouseup", function() {
-		controller.data["b"] = 0;
-		controller.sendState();
-	});
+	bDiv.setAttribute("onmousedown", "bClick()");
+	bDiv.setAttribute("onmouseup", "bRelease()");
 	controller.addHTMLObject(bDiv);
+	controller.setup();
+}
+
+function leftClick(){
+	controller.data["left"] = 1;
+	controller.sendState();
+}
+
+function leftRelease(){
+	controller.data["left"] = 0;
+	controller.sendState();
+}
+
+function rightClick(){
+	controller.data["right"] = 1;
+	controller.sendState();
+}
+
+function rightRelease(){
+	controller.data["right"] = 0;
+	controller.sendState();
+}
+
+function upClick(){
+	controller.data["up"] = 1;
+	controller.sendState();
+}
+
+function upRelease(){
+	controller.data["up"] = 0;
+	controller.sendState();
+}
+
+function downClick(){
+	controller.data["down"] = 1;
+	controller.sendState();
+}
+
+function downRelease(){
+	controller.data["down"] = 0;
+	controller.sendState();
+}
+
+function aClick(){
+	controller.data["a"] = 1;
+	controller.sendState();
+}
+
+function aRelease(){
+	controller.data["a"] = 0;
+	controller.sendState();
+}
+
+function bClick(){
+	controller.data["b"] = 1;
+	controller.sendState();
+}
+
+function bRelease(){
+	controller.data["b"] = 0;
+	controller.sendState();
 }
 
 window.onload = function(){
